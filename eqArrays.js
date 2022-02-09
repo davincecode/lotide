b19f42f6bdfdc64b394e31052b1d5523ac0f62c4
davincecode <hyperscript.ca@gmail.com>
2022-02-09 06:02:54 +0000

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -7,29 +7,17 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = (arr1, arr2) => {
-
-  let result1 = [];
-  let result2 = [];
-
-
-  for (let items of arr1) {
-    result1 += items;
-
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
+      return false;
+    }
   }
-  for (let items of arr2) {
-    result2 += items;
-
-  }
-
-  if (result1 === result2) {
-    return true;
-  } else {
-    return false;
-
-  }
-
+  return true;
 };
 
+console.log(eqArrays([1,2,3],[1,2,3]));
+
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));    // => should PASS
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));    // => should PASS
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true));    // => should Fail
 console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));    // => should PASS
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true));    // => should Fail
