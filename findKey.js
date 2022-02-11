08f0c2d7b1f398b1cd82c9c85282a022ce992911
davincecode <hyperscript.ca@gmail.com>
2022-02-11 06:50:35 +0000

    findKey.js

---
+++ findKey.js
@@ -0,0 +1,35 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (obj, callback) => {
+  let result = [];
+
+  for (const item in obj) {
+    if (callback(obj[item])) {
+      result = item;
+      console.log(result);
+      return result;
+    }
+  }
+  return result;
+};
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
+);
