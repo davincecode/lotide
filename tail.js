6f99c5b3090eb70030e49b43c9e61d19616c1cd5
davincecode <hyperscript.ca@gmail.com>
2022-02-14 06:18:38 +0000

    refactored

--- tail.js
+++ tail.js
@@ -1,15 +1,18 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected)
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+const assertEqual = require("./assertEqual");
+
+// const tail = (array) => {
+//   let newArray = array.slice(-1);
+//   return newArray;
+// };
+
+const _ = require("lodash");
+
+const tail = (arr) => {
+  return _.last(arr);
 };
 
-const tail = (array) => {
-  let newArray = array.slice(-1);
-  return newArray;
-};
+console.log(assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), "Labs"));
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
