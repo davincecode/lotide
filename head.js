e8e967a63e316134d735cff7f65a240e83deaab5
davincecode <hyperscript.ca@gmail.com>
2022-02-14 06:12:37 +0000

    refactored

--- head.js
+++ head.js
@@ -1,15 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `✅✅✅${actual} === ${expected}`;
-  } else {
-    return `🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
+const assertEqual = require("./assertEqual");
 
-const head = function(array) {
-  let firsElement = array[0];
-  return firsElement;
-};
+const _ = require("lodash");
 
-console.log(assertEqual(head([5,6,7]), 5));
+const head = (arr) => {
+  return _.first(arr);
+};
+console.log(assertEqual(head([5, 6, 7]), 5));
 console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
