fb9b1da26839e65ac2d4b1ddf374f458cab89cc2
davincecode <hyperscript.ca@gmail.com>
2022-02-14 03:53:22 +0000

    lotide

---
+++ map.js
@@ -0,0 +1,33 @@
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅  Assertion passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, (word) => word[0]);
+console.log(assertArraysEqual(results1, ["g", "c", "t", "m", "t"]));
