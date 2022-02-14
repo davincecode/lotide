1de96027a077c7f90ad83c70c2549714efc56c07
davincecode <hyperscript.ca@gmail.com>
2022-02-14 20:39:12 +0000

    refactored lotide

--- head.js
+++ head.js
@@ -1,9 +1,7 @@
-const assertEqual = require("./assertEqual");
-
 const _ = require("lodash");
 
 const head = (arr) => {
   return _.first(arr);
 };
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
