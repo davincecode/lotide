759ca2e206820c18af35a4e4ca426fa2cb1d18e0
davincecode <hyperscript.ca@gmail.com>
2022-02-15 02:27:57 +0000

    implemented to Object Property Value Shorthand

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const tail = require("./tail");
 const middle = require("./middle");
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
+  head,
+  tail,
+  middle, // a comma won't throw an error since it's a new feature in ES6.
 };
+
+//refactored to Object property value shortHand
